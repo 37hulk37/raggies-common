6678411b37babeef3761baf8e91f5c3c7312199b
Alexander Ushakov <alex.03spb@mail.ru>
2026-06-02 14:46:18 +0300

    added tariffs

--- src/main/kotlin/ModuleAutoConfiguration.kt
+++ src/main/kotlin/ModuleAutoConfiguration.kt
@@ -1,4 +1,6 @@
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
 @AutoConfiguration
+@ComponentScan(value = ["exception"])
 open class ModuleAutoConfiguration
